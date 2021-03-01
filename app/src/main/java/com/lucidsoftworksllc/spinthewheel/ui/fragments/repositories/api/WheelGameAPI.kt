defb1d5b9c3bf99446ddb96f06a9ba7f94131276
Brendan Abbott <lucidsoftworksllc@gmail.com>
2021-03-01 15:27:43 -0500

    Custom API created to test different size array responses.

--- app/src/main/java/com/lucidsoftworksllc/spinthewheel/ui/fragments/repositories/api/WheelGameAPI.kt
+++ app/src/main/java/com/lucidsoftworksllc/spinthewheel/ui/fragments/repositories/api/WheelGameAPI.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface WheelGameAPI {
 
-    @GET("539dc092-8367-414a-8892-ed3b2d666dbe")
+    @GET("539dc092-8367-414a-8892-ed3b2d666dbe.php")
     suspend fun getWheelInfo(): WheelSpinnerResponseModel
 
 }
